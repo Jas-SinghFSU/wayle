020774572c68948017ed842b1418ebac4678d127
Jas Singh <jaskiratpal.singh@outlook.com>
2026-03-28 13:33:01 -0700

    integrate deferred bluetooth service handling in bluetooth module

--- crates/wayle-shell/src/shell/bar/modules/bluetooth/messages.rs
+++ crates/wayle-shell/src/shell/bar/modules/bluetooth/messages.rs
@@ -2,13 +2,14 @@ use std::{rc::Rc, sync::Arc};
 
 use wayle_bluetooth::BluetoothService;
 use wayle_config::ConfigService;
+use wayle_core::DeferredService;
 use wayle_widgets::prelude::BarSettings;
 
 use crate::shell::bar::dropdowns::DropdownRegistry;
 
 pub(crate) struct BluetoothInit {
     pub settings: BarSettings,
-    pub bluetooth: Arc<BluetoothService>,
+    pub bluetooth: DeferredService<BluetoothService>,
     pub config: Arc<ConfigService>,
     pub dropdowns: Rc<DropdownRegistry>,
 }
@@ -25,6 +26,7 @@ pub(crate) enum BluetoothMsg {
 #[derive(Debug)]
 #[allow(clippy::enum_variant_names)]
 pub(crate) enum BluetoothCmd {
+    ServiceReady(Arc<BluetoothService>),
     StateChanged,
     IconConfigChanged,
     AdapterChanged,
